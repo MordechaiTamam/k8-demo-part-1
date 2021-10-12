8d8943632ac20b590a7e9a3cdb863abf91fc6b92
modi <modi.tamam@gmail.com>
2021-10-12 18:06:09 +0300

    Lecture 10 content

--- flask-redis/app.py
+++ flask-redis/app.py
@@ -6,9 +6,12 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-REDIS_MASTER_HOST = os.environ['REDIS_MASTER_HOST']
+REDIS_MASTER_HOST = os.environ.get('REDIS_MASTER_SERVICE_HOST')
 cache = redis.Redis(host=REDIS_MASTER_HOST, port=6379)
-
+import socket
+my_ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1],
+                   [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in
+                     [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 def get_hit_count():
     retries = 5
     while True:
@@ -23,4 +26,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello World! I have been seen {} times.\n my ip: {}'.format(count,my_ip)
